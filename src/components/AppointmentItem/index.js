c4dac7f78290b0aff72039cbfe30c8d3f9458567
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-02-14 20:54:23 +0530

    testing

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -1,1 +1,31 @@
 // Write your code here
+
+import './index.css'
+
+const AppointmentItem = props => {
+  const {appointment, markAsFavorite} = props
+  const {id, title, date, isStared} = appointment
+
+  const markAsStared = () => {
+    markAsFavorite(id)
+  }
+
+  const updateImage = isStared
+    ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+
+  return (
+    <li className="activity-container">
+      <div className="activity">
+        <p className="activity-name"> {title} </p>
+        <button className="start-btn" type="button" onClick={markAsStared}>
+          <img src={updateImage} alt="star" className="star-logo" />
+        </button>
+      </div>
+      <p className="activity-date"> {date} </p>
+    </li>
+  )
+}
+
+export default AppointmentItem
+
