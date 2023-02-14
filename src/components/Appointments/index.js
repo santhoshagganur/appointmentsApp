3e2cb5553765e2e6d873667548b0a9e491404c96
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-02-14 20:57:19 +0530

    testing

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,15 +1,70 @@
 // Write your code here
+import {v4 as uuidv4} from 'uuid'
+import {format} from 'date-fns'
 import {Component} from 'react'
+import AppointmentItem from '../AppointmentItem'
+
 import './index.css'
 
 class Appointments extends Component {
+  state = {title: '', date: '', appointmentsList: []}
+
+  addToList = event => {
+    event.preventDefault()
+    const {title, date} = this.state
+
+    const newAppointment = {
+      id: uuidv4(),
+      title,
+      date,
+      isStared: false,
+    }
+
+    this.setState(prevState => ({
+      appointmentsList: [...prevState.appointmentsList, newAppointment],
+      title: '',
+      date: '',
+    }))
+  }
+
+  changeTitle = event => {
+    this.setState({title: event.target.value})
+  }
+
+  changeDate = event => {
+    const appointmentDate = format(
+      new Date(event.target.value),
+      'dd MMMM yyyy, EEEE',
+    )
+    this.setState({date: appointmentDate})
+    console.log(appointmentDate)
+  }
+
+  markAsFavorite = id => {
+    this.setState(prevState => ({
+      appointmentsList: prevState.appointmentsList.map(each => {
+        if (each.id === id) {
+          return {...each, isStared: !each.isStared}
+        }
+        return each
+      }),
+    }))
+  }
+
+  getStaredItems = () => {
+    const {appointmentsList} = this.setState
+    const filteredData = appointmentsList.filter(each => each.isStared === true)
+    this.setState({appointmentsList: filteredData})
+  }
+
   render() {
+    const {title, date, appointmentsList} = this.state
     return (
       <div className="bg-container">
         <div className="card-container">
           <div className="upper-part">
             <div>
-              <form className="form-control">
+              <form className="form-control" onSubmit={this.addToList}>
                 <h1 className="heading"> Add Appointment </h1>
                 <label htmlFor="title" className="label-el">
                   TITLE
@@ -19,15 +74,19 @@ class Appointments extends Component {
                   type="text"
                   placeholder="Title"
                   className="input-el"
+                  onChange={this.changeTitle}
+                  value={title}
                 />
                 <label htmlFor="date" className="label-el">
                   DATE
                 </label>
                 <input
                   id="date"
-                  type="Date"
+                  type="date"
                   placeholder="dd/mm/yyyy"
                   className="input-el"
+                  onChange={this.changeDate}
+                  value={date}
                 />
                 <button type="submit" className="add-btn">
                   Add
@@ -46,10 +105,23 @@ class Appointments extends Component {
           <div>
             <div className="bottom-heading">
               <h1 className="heading-2"> Appointments </h1>
-              <button className="mark-star" type="button">
+              <button
+                className="mark-star"
+                type="button"
+                onClick={this.getStaredItems}
+              >
                 Starred
               </button>
             </div>
+            <ul className="appointments-container">
+              {appointmentsList.map(each => (
+                <AppointmentItem
+                  appointment={each}
+                  key={each.id}
+                  markAsFavorite={this.markAsFavorite}
+                />
+              ))}
+            </ul>
           </div>
         </div>
       </div>
